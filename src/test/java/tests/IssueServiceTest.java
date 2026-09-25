15971f80ac1810e48d466055b227f24486ce6c75
Vasco Geada <geadavasco@gmail.com>
2026-09-25 17:19:39 +0100

    IssueServiceTest - better coding

--- src/test/java/tests/IssueServiceTest.java
+++ src/test/java/tests/IssueServiceTest.java
@@ -19,47 +19,44 @@ class IssueServiceTest {
 	@Test
 	void searchTitle_Test() {
 		Project p = new Project("new project");
-		p.addIssue( new Task("Implement LOGIN", "This description", Priority.LOW));
-		p.addIssue( new Task("Implement UI", "This description", Priority.CRITICAL));
-		p.addIssue( new Task("Fix login bug", "This description", Priority.MEDIUM));
-		p.addIssue( new Task("Create dashboard", "This description", Priority.HIGH));
-		
+		p.addIssue(new Task("Implement LOGIN", "This description", Priority.LOW));
+		p.addIssue(new Task("Implement UI", "This description", Priority.CRITICAL));
+		p.addIssue(new Task("Fix login bug", "This description", Priority.MEDIUM));
+		p.addIssue(new Task("Create dashboard", "This description", Priority.HIGH));
+
 		IssueService issueService = new IssueService();
-		
-		
+
 		List<Issue> list = issueService.searchByTitle(p, "login");
-		
-		assertTrue(list.size() == 2);
-		
-		
+
+		assertEquals(2, list.size());
 	}
-	
-	
+
 	@Test
 	void orderBy_Test() {
 		Project p = new Project("new project");
-		p.addIssue( new Task("Implement LOGIN", "This description", Priority.LOW));
+		p.addIssue(new Task("Implement LOGIN", "This description", Priority.LOW));
 
-		
 		IssueService issueService = new IssueService();
-		
-		/* ORDER BY WITH 1 TASK, CHECK IF RETURNS THE LIST WITH 1 ELEMENT */
-		
-		List<Issue> list =  issueService.orderByPriority(p);
-		System.out.println(list.size());
-		assertTrue(list.size() == 1);
 
-		/* ORDER BY WITH MULTIPLE TASKS, CHECK IF THE TASK ARE ORDERED BY PRIORITY (CRITICAL -> LOW)*/
-		p.addIssue( new Task("Implement UI", "This description", Priority.CRITICAL));
-		p.addIssue( new Task("Fix login bug", "This description", Priority.MEDIUM));
-		p.addIssue( new Task("Create dashboard", "This description", Priority.HIGH));
-		list =  issueService.orderByPriority(p);
+		/* ORDER BY WITH 1 TASK, CHECK IF RETURNS THE LIST WITH 1 ELEMENT */
+
+		List<Issue> list = issueService.orderByPriority(p);
 		
-		assertTrue(list.get(0).getPriority() == Priority.CRITICAL);
-		assertTrue(list.get(1).getPriority() == Priority.HIGH);
-		assertTrue(list.get(2).getPriority() == Priority.MEDIUM);
-		assertTrue(list.get(3).getPriority() == Priority.LOW);
-		
-		
+		assertEquals(1, list.size());
+
+		/*
+		 * ORDER BY WITH MULTIPLE TASKS, CHECK IF THE TASK ARE ORDERED BY PRIORITY
+		 * (CRITICAL -> LOW)
+		 */
+		p.addIssue(new Task("Implement UI", "This description", Priority.CRITICAL));
+		p.addIssue(new Task("Fix login bug", "This description", Priority.MEDIUM));
+		p.addIssue(new Task("Create dashboard", "This description", Priority.HIGH));
+		list = issueService.orderByPriority(p);
+
+		assertEquals(Priority.CRITICAL, list.get(0).getPriority());
+		assertEquals(Priority.HIGH, list.get(1).getPriority());
+		assertEquals(Priority.MEDIUM, list.get(2).getPriority());
+		assertEquals(Priority.LOW, list.get(3).getPriority());
+
 	}
 }
